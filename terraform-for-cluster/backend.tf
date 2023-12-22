9f0457ff9ba1d6e05646305e1d982eceaeba805c
Snewhadeep98 <snsa18cs@cmrit.ac.in>
2023-12-22 13:50:16 +0530

    Changes

--- terraform-for-cluster/backend.tf
+++ terraform-for-cluster/backend.tf
@@ -1,8 +1,8 @@
-//terraform {
- // backend "s3" {
-  //  bucket = "ncukpreprod-eks"
-   // region = "eu-west-3"
-    //key = "eks/terraform.tfstate"
-  //}
-//}
+terraform {
+  backend "s3" {
+    bucket = "ncukpreprod-eks"
+    region = "eu-west-3"
+    key = "eks/terraform.tfstate"
+  }
+}
 
